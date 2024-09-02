d5a8fa6ee1488477425f0b20b35da1ccad6c5de7
Pavel <tigerr.cat@gmail.com>
2024-09-02 23:36:39 +0300

    seeder changed

--- seeder/seed.ts
+++ seeder/seed.ts
@@ -14,12 +14,26 @@ const createUsers = async () => {
 		 role: 'PROVIDER', // Значение по умолчанию
 	  },
 	});
+
+	await prisma.user.upsert({
+	  where: { id: 7 },
+	  update: {}, // Не обновляем ничего, если пользователь уже существует
+	  create: {
+		 id: 7, // Указываем нужный ID (если не используем автогенерацию, убери это поле)
+		 role: 'PROVIDER', // Значение по умолчанию
+	  },
+	});
  }
 
 
 
 const createProducts = async (quantity: number) => {
 	const products: Product[] = []
+
+	// const useFirstBrand = Math.random() < 0.5;
+	// const brands = [brand, brand2];
+
+
 
 	// Используем upsert для создания или обновления категории
 	const category = await prisma.category.upsert({
@@ -34,31 +48,107 @@ const createProducts = async (quantity: number) => {
 	})
 
 	// Используем upsert для создания или обновления бренда
-	const brand = await prisma.brand.upsert({
-		where: { slug: 'lacoste-russia' },
-		update: {}, // Оставляем пустым, если не требуется обновление
-		create: {
-			name: 'Lacoste Россия',
-			slug: 'lacoste-russia',
-			city: 'Москва',
-			logoPath: '/uploads/brands/brand-1-logo.png',
-			about:
-				'Кстати, непосредственные участники технического прогресса формируют глобальную экономическую сеть и при этом — ограничены исключительно образом мышления...',
-			category: {
-				connect: {
-					id: category.id,
+	
+	// const brand = await prisma.brand.upsert({
+	// 	where: { slug: 'lacoste-russia' },
+	// 	update: {}, // Оставляем пустым, если не требуется обновление
+	// 	create: {
+	// 		name: 'Lacoste Россия',
+	// 		slug: 'lacoste-russia',
+	// 		city: 'Москва',
+	// 		logoPath: '/uploads/brands/brand-1-logo.png',
+	// 		about:
+	// 			'Кстати, непосредственные участники технического прогресса формируют глобальную экономическую сеть и при этом — ограничены исключительно образом мышления...',
+	// 		category: {
+	// 			connect: {
+	// 				id: category.id,
+	// 			},
+	// 		},
+	// 		user: {
+	// 			connect: {
+	// 				id: 6,
+	// 			},
+	// 		},
+	// 	},
+	// })
+
+	// const brand2 = await prisma.brand.upsert({
+	// 	where: { slug: 'lacoste-kazan' },
+	// 	update: {}, // Оставляем пустым, если не требуется обновление
+	// 	create: {
+	// 		name: 'Lcste Казань',
+	// 		slug: 'lacoste-kazan',
+	// 		city: 'Казань',
+	// 		logoPath: '/uploads/brands/brand-2-logo.png',
+	// 		about:
+	// 			'Кстати, непосредственные участники технического прогресса формируют глобальную экономическую сеть и при этом — ограничены исключительно образом мышления...',
+	// 		category: {
+	// 			connect: {
+	// 				id: category.id,
+	// 			},
+	// 		},
+	// 		user: {
+	// 			connect: {
+	// 				id: 7,
+	// 			},
+	// 		},
+	// 	},
+	// })
+
+	const brands = [
+		await prisma.brand.upsert({
+			where: { slug: 'lacoste-russia' },
+			update: {},
+			create: {
+				name: 'Lacoste Россия',
+				slug: 'lacoste-russia',
+				city: 'Москва',
+				logoPath: '/uploads/brands/brand-1-logo.png',
+				about:
+					'Кстати, непосредственные участники технического прогресса формируют глобальную экономическую сеть и при этом — ограничены исключительно образом мышления...',
+				category: {
+					connect: {
+						id: category.id,
+					},
+				},
+				user: {
+					connect: {
+						id: 6,
+					},
 				},
 			},
-			user: {
-				connect: {
-					id: 6,
+		}),
+		await prisma.brand.upsert({
+			where: { slug: 'lacoste-kazan' },
+			update: {},
+			create: {
+				name: 'Lac Казань',
+				slug: 'lacoste-kazan',
+				city: 'Казань',
+				logoPath: '/uploads/brands/brand-2-logo.png',
+				about:
+					'Кстати, непосредственные участники технического прогресса формируют глобальную экономическую сеть и при этом — ограничены исключительно образом мышления...',
+				category: {
+					connect: {
+						id: category.id,
+					},
+				},
+				user: {
+					connect: {
+						id: 7,
+					},
 				},
 			},
-		},
-	})
+		}),
+	]
+
 
 	// Генерация продуктов
 	for (let i = 0; i < quantity; i++) {
+		
+		const randomBrand = brands[Math.floor(Math.random() * brands.length)]
+
+
 		const product = await prisma.product.create({
 			data: {
 				name: `Продукт ${i}`,
@@ -74,27 +164,55 @@ const createProducts = async (quantity: number) => {
 					`/uploads/products/product-${i}-image-5.png`,
 					`/uploads/products/product-${i}-image-6.png`,
 				],
+				// prices: {
+				// 	createMany: {
+				// 		data: [
+				// 			{
+				// 				price: 2000,
+				// 				minQuantity: 2,
+				// 			},
+				// 			{
+				// 				price: 1000,
+				// 				minQuantity: 5,
+				// 			},
+				// 			{
+				// 				price: 800,
+				// 				minQuantity: 100,
+				// 			},
+				// 			{
+				// 				price: 500,
+				// 				minQuantity: 1000,
+				// 			},
+				// 		],
+				// 	},
+				// },
 				prices: {
 					createMany: {
 						data: [
 							{
-								price: 1000,
+								price: 2000 + i * 10, // Уникальная цена
+								minQuantity: 2,
+							},
+							{
+								price: 1000 + i * 5, // Уникальная цена
 								minQuantity: 5,
 							},
 							{
-								price: 800,
+								price: 800 + i * 2, // Уникальная цена
 								minQuantity: 100,
 							},
 							{
-								price: 500,
+								price: 500 + i * 1, // Уникальная цена
 								minQuantity: 1000,
 							},
 						],
 					},
 				},
+				
 				brand: {
 					connect: {
-						id: brand.id,
+						// id: useFirstBrand ? brand.id : brand2.id,
+						id: randomBrand.id, // Присваиваем случайно выбранный бренд
 					},
 				},
 				category: {
@@ -114,7 +232,7 @@ const createProducts = async (quantity: number) => {
 async function main() {
 	console.log('Start seeding ...')
 	await createUsers();
-	await createProducts(10)
+	await createProducts(20)
 }
 
 main()
